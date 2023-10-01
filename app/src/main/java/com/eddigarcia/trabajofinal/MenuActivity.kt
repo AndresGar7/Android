1cf016a0335bd1cffae01dbdbd9586a1a9443ea4
Eddi Garcia <eddy.garcia@keyandcloud.com>
2023-10-02 01:58:39 +0200

    Agregando Copyright (c) y Seccion de Heroes

--- app/src/main/java/com/eddigarcia/trabajofinal/MenuActivity.kt
+++ app/src/main/java/com/eddigarcia/trabajofinal/MenuActivity.kt
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.eddigarcia.trabajofinal.firstapp.FirstAppActivity
+import com.eddigarcia.trabajofinal.heroes.HeroesAppActivity
 import com.eddigarcia.trabajofinal.imccalculatorapp.ImcCalculatorAppActivity
 
 
@@ -24,9 +25,11 @@ class MenuActivity : AppCompatActivity() {
 
         val btnSaludoApp = findViewById<Button>(R.id.btnSaludoApp)
         val btnIMCApp = findViewById<Button>(R.id.btnIMCApp)
+        val btnHeroesApp = findViewById<Button>(R.id.btnHeroesApp)
 
         btnSaludoApp.setOnClickListener { navigateToSaludoApp() }
         btnIMCApp.setOnClickListener { navigateToIMCApp() }
+        btnHeroesApp.setOnClickListener { navigateToHeroesApp() }
     }
 
     private fun navigateToSaludoApp() {
@@ -39,4 +42,10 @@ class MenuActivity : AppCompatActivity() {
         val intent = Intent(this, ImcCalculatorAppActivity::class.java)
         startActivity(intent)
     }
+
+    private fun navigateToHeroesApp() {
+        val intent = Intent(this, HeroesAppActivity::class.java)
+        startActivity(intent)
+    }
+
 }
---
+++ app/src/main/java/com/eddigarcia/trabajofinal/heroes/HeroesAppActivity.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2023.
+ *
+ * Project Name: Trabajo Final
+ * Created by Eddi Andres Garcia
+ * Copyright (c) 2023. All rights reserved.
+ * Last modified 1/10/23 23:05
+ */
+
+package com.eddigarcia.trabajofinal.heroes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eddigarcia.trabajofinal.R
+
+class HeroesAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_heroes_app)
+    }
+}
